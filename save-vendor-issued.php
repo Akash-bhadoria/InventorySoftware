e6385eb8cba2b01be92f8d702bdee40a7052cbbd
Akash-bhadoria <akashbhadoria297@gmail.com>
2022-10-27 20:29:50 +0530

    add array name

---
+++ save-vendor-issued.php
@@ -0,0 +1,8 @@
+<?php
+    $vendor_id = $_POST['vendor_issued'];
+    $item_id = $_POST['item_name_issued'];
+    $quantity = $_POST['quantity_issued'];
+    $challan = $_POST['challan_issued'];
+    $date = $_POST['date_issued'];
+    print_r($item_id);
+    print_r($vendor_id);
